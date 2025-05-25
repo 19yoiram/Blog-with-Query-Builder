e84c000a5b16b197cc781465083ee7d95bd3b4e0
19yoiram <iramsahar7135@gmail.com>
2025-05-26 00:40:01 +0500

    taggable table

--- database/migrations/2025_05_25_193300_create_taggables_table.php
+++ database/migrations/2025_05_25_193300_create_taggables_table.php
@@ -11,8 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('taggable', function (Blueprint $table) {
+        Schema::create('taggables', function (Blueprint $table) {
             $table->id();
+            $table->morphs('taggable');
+            $table->foreignId('tag_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -22,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('taggable');
+        Schema::dropIfExists('taggables');
     }
 };
